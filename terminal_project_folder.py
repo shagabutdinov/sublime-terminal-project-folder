9b36f7dab37e6c1e85b2db8657edd021e9b467cf
Leonid Shagabutdinov <leonid@shagabutidnov.com>
2015-11-24 09:57:43 +0600

    error notification modified

--- terminal_project_folder.py
+++ terminal_project_folder.py
@@ -3,11 +3,11 @@ import sublime_plugin
 
 try:
   from Terminal.Terminal import OpenTerminalCommand
-except ImportError:
+except ImportError as error:
   sublime.error_message("Dependency import failed; please read readme for " +
     "TerminalProjectFolder plugin for installation instructions; to disable " +
-    "this message remove this plugin")
-
+    "this message remove this plugin; message: " + str(error))
+  raise error
 
 class OpenTerminalInProjectFolderEnhanced(sublime_plugin.WindowCommand):
   def run(self, parameters = None):
